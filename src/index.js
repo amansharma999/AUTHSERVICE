6f6074338071c8e782cd483ecd3bcad5b9ca8501
amansharma999 <aman.gamestar999@gmail.com>
2024-09-16 04:37:07 +0530

    Added roles and userRoles through table commit fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const app = express();
 const { PORT } = require("./config/serverConfig");
 const apiRoutes = require("./routes/index");
-const { User, Role } = require("./models/index");
 
 const prepareAndStartServer = () => {
   app.use(bodyParser.json());
@@ -15,9 +14,6 @@ const prepareAndStartServer = () => {
       const db = require("./models/index");
       db.sequelize.sync({ alter: true });
     }
-    const u1 = await User.findByPk(4);
-    const r1 = await Role.findByPk(1);
-    u1.addRole(r1);
   });
 };
 
